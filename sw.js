120762e5c77d6fe27679b55fa791326d1c69eac6
virgilinifranchesco72-tech <virgilinifranchesco72@gmail.com>
2026-08-15 22:01:06 -0300

    Refresh full armor 3D interface cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stark-armor-lab-v2';
+const CACHE='stark-armor-lab-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
